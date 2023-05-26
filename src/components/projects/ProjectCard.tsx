64d3281ae139e7591e97cf63aa35bd64fb72f95c
pokemau <rentillosa90@gmail.com>
2023-05-26 16:11:05 +0800

    add images to projects

---
+++ src/components/projects/ProjectCard.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { ProjectsList } from "./ProjectsList";
+import { BsStack } from "react-icons/bs";
+import Image from "next/image";
+
+const ProjectCard = () => {
+  return (
+    <>
+      {ProjectsList &&
+        ProjectsList.map((project) => (
+          <div
+            key={project.id}
+            className="bg-main-bg w-[95%] md:w-[70%] rounded-md py-4 px-4 md:px-6
+             transition-all duration-150">
+            {project.img && (
+              <Link
+                href={project.site}
+                target="_blank"
+                rel="noopener norefferer"
+                className="block w-full md:w-[67%] mx-auto hover:scale-[1.01] 
+                transition-all max-w-[35rem]">
+                <Image
+                  src={project.img}
+                  alt={project.id}
+                  className="w-full mb-4 aspect-auto rounded-md
+                  shadow-[0px_5px_20px_1px_#2b2b2a]"
+                />
+              </Link>
+            )}
+            <div className="flex justify-between">
+              <Link
+                href={project.site}
+                target="_blank"
+                rel="noopener norefferer">
+                <h1 className="text-xl text-yel hover:text-yel-hover font-[700] mb-2">
+                  {project.name}
+                </h1>
+              </Link>
+            </div>
+            <p>{project.desc}</p>
+
+            <div className="flex items-center justify-between mt-4 flex-wrap gap-4">
+              <div className="flex items-center gap-2 flex-wrap">
+                <BsStack />
+                {project.techUsed.map((tech) => (
+                  <p>{tech}</p>
+                ))}
+              </div>
+              <div className="flex gap-4 text-[1.4rem]">
+                {project.links.map((link) => (
+                  <Link
+                    href={link.site}
+                    className="hover:text-gray-400"
+                    target="_blank"
+                    rel="noopener norefferer">
+                    <link.icon />
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default ProjectCard;
---
+++ src/components/projects/ProjectsList.ts
@@ -0,0 +1,110 @@
+import { AiFillGithub } from "react-icons/ai";
+import { FiExternalLink } from "react-icons/fi";
+
+import yahalloImg from "../../images/chatapp.png";
+import flashcardoImg from "../../images/flaschardo1.png";
+import lolChampaImg from "../../images/lolchamps.png";
+import sergdleImg from "../../images/sergdle.png";
+
+export const ProjectsList = [
+  {
+    id: "yahallo-chat-app",
+    name: "Yahallo",
+    desc: "A website for making flashcards to help the user study for an upcoming quiz or exam. It uses the browser's local storage to store the flashcard sets which can be deleted by the user.",
+    site: "https://yahallo.vercel.app/",
+    techUsed: ["NEXTJS", "FIREBASE", "TAILWIND"],
+    links: [
+      {
+        site: "https://github.com/pokemau/Chat-App",
+        icon: AiFillGithub,
+      },
+      {
+        site: "https://yahallo.vercel.app/",
+        icon: FiExternalLink,
+      },
+    ],
+    img: yahalloImg,
+  },
+
+  {
+    id: "flashcardo",
+    name: "Flashcardo",
+    site: "https://flashcardo.vercel.app/",
+    desc: "A website for making flashcards to help the user study for an upcoming quiz or exam. It uses the browser's local storage to store the flashcard sets which can be deleted by the user.",
+    techUsed: ["NEXTJS", "TAILWIND", "HTML"],
+    links: [
+      {
+        site: "https://github.com/pokemau/Flashcardo-2",
+        icon: AiFillGithub,
+      },
+      {
+        site: "https://flashcardo.vercel.app/",
+        icon: FiExternalLink,
+      },
+    ],
+    img: flashcardoImg,
+  },
+
+  {
+    id: "lol-champs",
+    name: "LOL Champa",
+    site: "https://lolchamps-dusky.vercel.app/",
+    techUsed: ["REACTJS", "HTML", "CSS"],
+    desc: "A web app made with ReactJS which shows the basic information of a champion in the famous MOBA game League of Legends. The champion data is fetched from Riot Games's Data Dragon API.",
+    links: [
+      {
+        site: "https://github.com/pokemau/React-LOL-Champs",
+        icon: AiFillGithub,
+      },
+      {
+        site: "https://lolchamps-dusky.vercel.app/",
+        icon: FiExternalLink,
+      },
+    ],
+    img: lolChampaImg,
+  },
+
+  {
+    id: "sergdle",
+    name: "Sergdle",
+    site: "https://sergdle.vercel.app/",
+    techUsed: ["TYPESCRIPT", "TAILWIND"],
+    desc: "Sergdle is a Wordle clone, where the player has to guess the hidden word within 6 guesses to win the game.",
+    links: [
+      {
+        site: "https://github.com/pokemau/sergdle",
+        icon: AiFillGithub,
+      },
+      {
+        site: "https://sergdle.vercel.app/",
+        icon: FiExternalLink,
+      },
+    ],
+    img: sergdleImg,
+  },
+  // {
+  //   id: "lawfirm-site",
+  //   name: "Law Firm Website",
+  //   site: "https://lepitenbojos.org/",
+  //   techUsed: ["WORDPRESS"],
+  //   desc: "This is a mobile responsive website for a law firm located in Cebu City. I made this website with WordPress when I was an intern for them.",
+  //   links: [{ site: "https://lepitenbojos.org/", icon: FiExternalLink }],
+  // },
+  // {
+  //   id: "insta-clone",
+  //   name: "Instagram Clone",
+  //   site: "/",
+  //   techUsed: ["HTML", "CSS"],
+  //   desc: "A simple site made with plain CSS and HTML to apply my skills on following a given template or design for a website.",
+  //   links: [
+  //     {
+  //       site: "https://github.com/pokemau/Instagram-Clone",
+  //       icon: AiFillGithub,
+  //     },
+  //     {
+  //       site: "https://pokemau.github.io/instagram-clone/",
+  //       icon: FiExternalLink,
+  //     },
+  //   ],
+  // },
+];
