7e107e5ee4394461b5d55f21af1b12ec3e8d1a28
pokemau <rentillosa90@gmail.com>
2023-06-04 19:41:41 +0800

    change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { Layout } from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
